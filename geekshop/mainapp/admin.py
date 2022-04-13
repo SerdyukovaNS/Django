a2f89caf14086b97e2c6226e189e9c8ce490f9da
Nataliya <serdyukova.n.s@inbox.ru>
2022-04-13 16:49:33 +0300

    homework 8

--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -4,4 +4,12 @@ from django.contrib import admin
 from mainapp.models import Product, ProductCategories
 
 admin.site.register(ProductCategories)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'descriptions', ('price', 'quantity'), 'category')
+    readonly_fields = ('descriptions',)
+    ordering = ('name', 'price')
+    search_fields = ('name',)
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,6 +3,8 @@ import json
 
 from django.shortcuts import render
 from mainapp.models import Product, ProductCategories
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic import DetailView
 
 
 # Create your views here.
@@ -20,17 +22,32 @@ def index(request):
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request):
+def products(request, id_category=None, page=1):
 
-    # products = read_file('fixtures/goods.json')
-    # categories = read_file('fixtures/categories.json')
+    if id_category:
+        products = Product.objects.filter(category_id=id_category)
+
+    else:
+        products = Product.objects.all()
+
+    pagination = Paginator(products, per_page=2)
+
+    try:
+        product_pagination = pagination.page(page)
+    except PageNotAnInteger:
+        product_pagination = pagination.page(1)
+    except EmptyPage:
+        product_pagination = pagination.page(1)
 
     content = {
         'title': 'Geekshop - Каталог',
         'categories': ProductCategories.objects.all(),
-        'products': Product.objects.all()
+        'products': product_pagination
     }
 
     return render(request, 'mainapp/products.html', content)
 
 
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'mainapp/detail.html'
