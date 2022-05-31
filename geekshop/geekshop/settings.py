31bc1e41d6f9ca43e05996d33aed55cb95e82898
Nataliya <serdyukova.n.s@inbox.ru>
2022-05-31 13:19:16 +0300

    lesson_2_7

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # 'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,7 +69,8 @@ MIDDLEWARE = [
 
 
     'social_django.middleware.SocialAuthExceptionMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'django.middleware.cache.FetchFromCacheMiddleware'
 
 ]
 
